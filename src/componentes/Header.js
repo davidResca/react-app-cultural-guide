d03941d21aa6a51efb62df381694cb1b4fb6723d
davidResca <davidresca9@gmail.com>
2022-06-02 20:42:08 -0300

    Agrega Navbar y estilos

---
+++ src/componentes/Header.js
@@ -0,0 +1,10 @@
+import Navbar from './Navbar';
+
+
+const Header = () => {
+    return (
+    <Navbar />
+    )
+}
+
+export default Header;
---
+++ src/componentes/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1 className="navbar__title">guia cultural</h1>
+            <a className="navbar__link" href="#">inicio</a>
+            <a className="navbar__link" href="#">comprá tus entradas</a>
+            <a className="navbar__link" href="#">beneficios</a>
+            <a className="navbar__link" href="#">nosotros</a>
+        </nav>
+    )
+}
+export default Navbar;
