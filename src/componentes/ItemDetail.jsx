fe088119803455bfffbb0679f6f9f666c34f4d07
davidResca <davidresca9@gmail.com>
2022-06-16 23:57:08 -0300

    agrega itemDetail y estilos

--- src/componentes/ItemDetail.jsx
+++ src/componentes/ItemDetail.jsx
@@ -8,7 +8,7 @@ const ItemDetail = ({item}) => {
             <h1 className='detail-title'>{item.nombre}</h1>
             <h2 className='detail-title'>{item.autor}</h2>
             <img className='detail-img' src={item.img} alt={item.nombre} />
-            <strong className='detail-img'>{item.precio} ETH</strong>
+            <strong className='detail-img'>Precio: {item.precio} ETH</strong>
             <ItemCount stock={item.stock} initial={1} onAdd={()=>{}}/>
         </div>
     )
