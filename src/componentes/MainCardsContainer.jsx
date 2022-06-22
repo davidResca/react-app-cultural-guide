20e7b69ccf446e894352b0ca77fb567b19ea1593
davidResca <davidresca9@gmail.com>
2022-06-21 23:51:14 -0300

    agrega fetch de tarjeta en main

--- src/componentes/MainCardsContainer.jsx
+++ src/componentes/MainCardsContainer.jsx
@@ -1,12 +1,19 @@
 import CardPromoContainer from "./CardPromoContainer";
-import CardDisciplineContainer from "./CardDisciplineContainer";
+
 
 const MainCardsContainer = () => {
     return (
-        <div>
-            <CardPromoContainer />
-            <CardDisciplineContainer />
-        </div>
+        <divn className="MainCardsContainer">
+            <div className="main-title-container">
+            <h1 className="main-title">bienvenidx</h1>
+            <p className="main-subtitle">Esto es una guía</p>
+            <p className="main-subtitle-color">Cultural</p>
+            <p className="main-description">Compra tus entradas!</p>
+            </div>
+            <div className="cards-container">
+                <CardPromoContainer />
+            </div>
+        </divn>
     )
 }
 
--- src/componentes/base/newsData.js
+++ src/componentes/base/newsData.js
@@ -1,6 +1,6 @@
 const newsData = {
     id: 501,
-    img: "https://imagizer.imageshack.com/v2/375x500q90/922/bziWQS.png",
+    img: "https://imagizer.imageshack.com/v2/286x369q90/922/trmTUf.png",
     titulo: 'Caos y erupción! en La Plata',
     descripcion: 'La mítica banda vuelve a la ciudad de las diagonales con todo el metal para destruir el escenario por completo.'
 }
