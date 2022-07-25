2705cf8f0fd73e886c2ef4653d9d721a3d624d22
davidResca <davidresca9@gmail.com>
2022-07-25 02:47:19 -0300

    Agrega firebase itemColl I y II

--- src/componentes/Checkout.jsx
+++ src/componentes/Checkout.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
+import Form from './Form';
 import { useContext, useState } from 'react'
 import { contexto } from "../context/CartContext"
 import { Link } from 'react-router-dom';
 import { db } from "../firebase/firebase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 function Checkout() {
-    const { carrito, precioTotal } = useContext(contexto)
+    const { carrito, precioTotal, vaciarCarrito, eliminarProducto } = useContext(contexto)
     const [ idCompra, setIdCompra ] = useState("")
-    const [ mostrarModal, setMostrarModal ] = useState(false)
     const [usuario, setUsuario] = useState({
         nombre: "",
         apellido: "",
@@ -17,65 +17,59 @@ function Checkout() {
         confirmar_email: "",
     });
 
-    const orderDate = new Date().toLocaleDateString();
+    const { nombre, apellido, tel, email, confirmar_email } = usuario;
+
 
     const handleChange = (e) => {
+        const { id, value } = e.target;
         const copia = {...usuario};
-        copia[e.target.id] = e.target.value;
+        copia[id] = value;
         setUsuario(copia);
     };
+    //console.log(usuario);
 
-    function handleOrder() {
-        const order = {
-            nombre: "",
-            item: carrito,
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const orden = {
+            buyer: {
+                nombre: nombre,
+                apellido: apellido,
+                telefono: tel,
+                email: email,
+            },
+            carrito,
             precio: precioTotal,
-            fecha: orderDate,
+            fecha: serverTimestamp(),
         };
+
+        const ref = collection(db, 'ordenes');
+        
+        addDoc(ref, orden)
+        .then((response) => {
+            console.log(response)
+        });
+
     }
 
-    const addOrder = (order) => {
-        const doc = addDoc(collection(db, 'orders', order))
-        return doc.id;
-    }
-
-    if (idCompra !== '') {
+    /* if (idCompra !== '') {
         return (
-            <div className='page page__in page__center'>
-                <h2 >¡Muchas gracias por tu compra {(usuario.nombre).toUpperCase()}!</h2>
-                <p>Te enviamos un mail a {(usuario.email).toLowerCase()}</p>
+            <div className='checkout'>
+                <h2 >¡Muchas gracias por tu compra {(usuario.nombre)}!</h2>
+                <p>Te enviamos un mail a {(usuario.email)}</p>
                 <p> Con tu orden de compra ID: {idCompra}. </p> 
                 <p> ¡Hasta la próxima!</p>
                 <Link to='/'><button>volver al inicio</button></Link>
             </div>
         )
-    }
-
+    } */
     return (
         <div className='checkout'>
             <h2>Complete con sus datos para finalizar la compra</h2>
-            <form className='cart-form' action="">
-                <div className='form-input'>
-                    <input onChange={handleChange} placeholder="nombre..." type="text" id="nombre" value={usuario.nombre}/>
-                </div>
-                <div className='form-input'>
-                    <input onChange={handleChange} placeholder="nombre..." type="text" id="apellido" value={usuario.apellido}/>
-                </div>
-                <div className='form-input'>
-                    <input onChange={handleChange} placeholder="tel..." type="number" id="tel" value={usuario.tel}/>
-                </div>
-                <div className='form-input'>
-                    <input onChange={handleChange}  placeholder="email..." type="email" id="email" value={usuario.email}/>
-                </div>
-                <div className='form-input'>
-                    <input onChange={handleChange}  placeholder="confirmar email..." type="email" id="confirmar_email" value={usuario["confirmar_email"]}/>
-                </div>
-            </form>
-            { 
-            usuario.nombre && usuario.apellido && usuario.tel && usuario.email && usuario["confirmar_email"] && (usuario.email === usuario["confirmar_email"])
-                ? (<input onClick={() => { handleOrder(); setMostrarModal(true) }} type="submit" value="Proceder al pago" />) 
-                : (<input className='btnconf' type="submit" value="Proceder al pago" disabled />)
-            }
+            < Form 
+                handleChange = {handleChange} 
+                usuario = {usuario}
+                handleSubmit = { handleSubmit }
+            />
         </div>
     )
 }
--- src/componentes/Form.jsx
+++ src/componentes/Form.jsx
@@ -1,43 +1,56 @@
-import React from 'react'
-import { useState } from 'react';
 
-const Form = () => {
 
-    const [usuario, setUsuario] = useState({
-        nombre: "",
-        email: "",
-        tel: "",
-        tarjeta: "",
-    })
-
-    const handleSubmit = (e)=> {
-        e.preventDefault();
-        console.log(usuario);
-    }
-    
-    const handleChange = (e) => {
-        const copia = {...usuario};
-        copia[e.target.id] = e.target.value;
-        setUsuario(copia);
-    }
-
+const Form = ({handleChange, usuario, handleSubmit }) => {
+    //destructuring usuario.
+    const {nombre, apellido, tel, email, confirmar_email} = usuario;
     return (
-        <form className='cart-form' action="" onSubmit={handleSubmit}>
-            <div className='form-input'>
-                <input onChange={handleChange}  placeholder="nombre..." type="text" id="nombre" value={usuario.nombre}/>
-            </div>
-            <div className='form-input'>
-                <input onChange={handleChange}  placeholder="email..." type="email" id="email" value={usuario.email}/>
-            </div>
-            <div className='form-input'>
-                <input onChange={handleChange} placeholder="tel..."  type="number" id="tel" value={usuario.tel}/>
-            </div>
-            <div className='form-input'>
-                <input onChange={handleChange} placeholder="numero de tarjeta..."  type="number" id="tarjeta" value={usuario.tarjeta}/>
-            </div>
-            <button>comprar</button>
+        <form onSubmit = {handleSubmit} action="">
+            <input
+                className='form-input' 
+                onChange={handleChange} 
+                placeholder="nombre..." 
+                type="text" 
+                id="nombre" 
+                value={nombre}
+            />
+            <input 
+                className='form-input'
+                onChange={handleChange} 
+                placeholder="apellido..." 
+                type="text" 
+                id="apellido" 
+                value={apellido}
+            />
+            <input
+                className='form-input'
+                onChange={handleChange} 
+                placeholder="tel..." 
+                type="number" id="tel" 
+                value={tel}
+            />
+            <input 
+                className='form-input'
+                onChange={handleChange}  
+                placeholder="email..." 
+                type="email" 
+                id="email" 
+                value={email}
+            />
+            <input
+                className='form-input'
+                onChange={handleChange}  
+                placeholder="confirmar email..." 
+                type="email" 
+                id="confirmar_email" 
+                value={confirmar_email}
+            />
+            { nombre && apellido && tel && email && confirmar_email && (email === confirmar_email) ?
+            <button type="submit">Proceder al Pago</button>
+            : 
+            <button type="submit" disabled> Proceder al Pago</button>
+        }
         </form>
     )
 }
 
-export default Form
+export default Form;
